4c313496a86626b8fbd8a0ffaf56b033ed008bb4
Operator873 <operator873@873gear.com>
2021-01-22 11:41:21 -0600

    Create SAMtools with anticipated imports and function names.

---
+++ SAMtools.py
@@ -0,0 +1,43 @@
+import sqlite3
+import requests
+import json
+import re
+from requests_oauthlib import OAuth1
+
+def spambot():
+    pass
+
+
+def lock():
+    pass
+
+
+def gblock():
+    pass
+
+
+def massblock():
+    # Is this needed?
+    pass
+
+
+def masslock():
+    # Is this needed?
+    pass
+
+
+def massgblock():
+    # Is this needed?
+    pass
+
+
+def unlock():
+    pass
+
+
+def addoauth():
+    pass
+
+
+def addapi():
+    pass
